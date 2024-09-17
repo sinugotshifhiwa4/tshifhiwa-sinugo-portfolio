8780d7a50225748480603dff6afee69768a288aa
Tshifhiwa <tshifhiwa@comcorp.co.za>
2024-09-17 12:12:06 +0200

    Tshifhiwa Portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'home', 'title': 'Home', component:HomeComponent},
+    {'path': 'about', 'title': 'About', component:AboutComponent},
+    {'path': 'experience', 'title': 'Experience', component:ExperienceComponent},
+    {'path': 'projects', 'title': 'Projects', component:ProjectsComponent},
+    {'path': 'contact', 'title': 'Contact', component:ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
